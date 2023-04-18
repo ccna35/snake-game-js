38715903e5741a4a36a654d107e6f38af7d74b24
ccna35 <shawkykhalil@gmail.com>
2023-04-18 23:40:28 +0200

    "Snake game"

--- app.js
+++ app.js
@@ -1,110 +1,200 @@
-const container = document.querySelector(".container");
-const snake = document.querySelector(".snake");
+const grid = document.querySelector(".grid");
 const scoreBoard = document.querySelector(".score");
+const speedBoard = document.querySelector(".speed");
+const selectLevel = document.querySelector("#level");
+const startBtn = document.querySelector(".start");
+const resetBtn = document.querySelector(".reset");
 
-let collided = false;
-let direction = "right";
-let current = 1;
+console.log(speedBoard);
+
+let snakeX, snakeY;
+snakeX = snakeY = 10;
+let foodX, foodY;
+let snakeBody = [];
 let score = 0;
+let gameLevel = 500;
+let gamePlaying = false;
 
-let x = 0;
-let y = 0;
+let directionX = 1;
+let directionY = 0;
 
-// code for the bait
-const bait = document.createElement("div");
-bait.classList.add("bait");
+speedBoard.innerText = gameLevel;
 
-function generateRandomNum() {
-  let randomNum = Math.floor(Math.random() * 25);
-  if (randomNum > 0) {
-    return randomNum;
-  } else {
-    return generateRandomNum();
+document.addEventListener("keydown", (e) => {
+  if (e.code === "ArrowRight" && directionX !== -1) {
+    directionX = 1;
+    directionY = 0;
+  } else if (e.code === "ArrowLeft" && directionX !== 1) {
+    directionX = -1;
+    directionY = 0;
+  } else if (e.code === "ArrowUp" && directionY !== 1) {
+    directionY = -1;
+    directionX = 0;
+  } else if (e.code === "ArrowDown" && directionY !== -1) {
+    directionY = 1;
+    directionX = 0;
+  }
+});
+
+function createSnake() {
+  let snake = document.createElement("div");
+  snake.classList.add("head");
+
+  snake.style.cssText = `grid-column: ${snakeX};
+    grid-row: ${snakeY}`;
+  grid.appendChild(snake);
+  return snake;
+}
+
+let loop;
+
+let snakeHead;
+
+const func = () => {
+  // Let the last segment of the snake have the coordinates of the next segment, so on and so forth.
+  if (snakeBody.length) {
+    for (let index = snakeBody.length - 1; index > 0; index--) {
+      snakeBody[index] = snakeBody[index - 1];
+    }
+  }
+
+  // the first element of the snakeBody array will register the coordinates of the head.
+  if (snakeBody.length) {
+    snakeBody[0] = [snakeX, snakeY];
+  }
+
+  snakeX += directionX;
+  snakeY += directionY;
+
+  snakeHead.style.cssText = `grid-column: ${snakeX};
+      grid-row: ${snakeY};`;
+
+  if (snakeBody.length) {
+    for (let index = 0; index < snakeBody.length; index++) {
+      document.querySelectorAll(".snake")[
+        index
+      ].style.cssText = `grid-column: ${snakeBody[index][0]};
+      grid-row: ${snakeBody[index][1]};`;
+    }
+  }
+
+  // detect when the snake hits the food
+  if (snakeX === foodX && snakeY === foodY) {
+    score++;
+    if (score === 1 && gameLevel === 500) {
+      gameLevel = 250;
+      speedBoard.innerText = gameLevel;
+      console.log("YES", gameLevel);
+    } else if (score === 2 && gameLevel === 250) {
+      gameLevel = 125;
+      speedBoard.innerText = gameLevel;
+      console.log("YES Again", gameLevel);
+    }
+    scoreBoard.innerText = score;
+    // increase the body of the snake by one block
+    snakeBody.push([foodX, foodY]);
+    document.querySelectorAll(".food")[0].classList.add("snake");
+    document.querySelectorAll(".food.snake")[0].classList.remove("food");
+
+    // create new food with random coordinates
+    newFood();
+  }
+
+  if (snakeX >= 30 || snakeY >= 30 || snakeX <= 0 || snakeY <= 0) {
+    clearInterval(loop);
+    loop = null;
+    snakeX = 10;
+    snakeY = 10;
+    score = 0;
+    scoreBoard.innerText = score;
+  }
+};
+
+function startGame() {
+  if (!gamePlaying) {
+    snakeHead = createSnake();
+
+    // grid.appendChild(snakeHead);
+    newFood();
+    loop = setInterval(func, gameLevel);
   }
 }
 
-console.log(generateRandomNum());
+const randomPosition = () => {
+  let posX = Math.floor(Math.random() * 30) + 1;
+  let posY = Math.floor(Math.random() * 30) + 1;
 
-bait.style.top = `${generateRandomNum()}rem`;
-bait.style.left = `${generateRandomNum()}rem`;
-
-container.appendChild(bait);
-
-console.log(bait.getBoundingClientRect());
-
-const func = () => {
-  if (direction === "right") {
-    x++;
-    snake.style.transform = `translate(${x}px, ${y}px)`;
-  } else if (direction === "left") {
-    x--;
-    snake.style.transform = `translate(${x}px, ${y}px)`;
-  } else if (direction === "up") {
-    y--;
-    snake.style.transform = `translate(${x}px, ${y}px)`;
-  } else {
-    y++;
-    snake.style.transform = `translate(${x}px, ${y}px)`;
+  for (let i = 0; i < snakeBody.length; i++) {
+    if (snakeBody[i][0] !== posX && snakeBody[i][1] !== posY) {
+      return [posX, posY];
+    } else {
+      return randomPosition();
+    }
   }
-  if (
-    snake.getBoundingClientRect().right >=
-      container.getBoundingClientRect().right ||
-    snake.getBoundingClientRect().bottom >=
-      container.getBoundingClientRect().bottom ||
-    snake.getBoundingClientRect().top <=
-      container.getBoundingClientRect().top ||
-    snake.getBoundingClientRect().left <= container.getBoundingClientRect().left
-  ) {
-    clearInterval(test);
-    x = 0;
-    y = 0;
-    collided = true;
-    score = 0;
-    scoreBoard.innerText = score;
-    console.log("Collision!");
-  }
-
-  if (
-    snake.getBoundingClientRect().y <=
-      bait.getBoundingClientRect().y + bait.getBoundingClientRect().height &&
-    snake.getBoundingClientRect().y >= bait.getBoundingClientRect().y &&
-    snake.getBoundingClientRect().x >= bait.getBoundingClientRect().x &&
-    snake.getBoundingClientRect().x <=
-      bait.getBoundingClientRect().x + bait.getBoundingClientRect().width
-  ) {
-    console.log("Bingo");
-    score++;
-    scoreBoard.innerText = score;
-    snake.style.width = `${snake.getBoundingClientRect().width + 16}px`;
-    bait.style.top = `${generateRandomNum()}rem`;
-    bait.style.left = `${generateRandomNum()}rem`;
-  }
-
-  current++;
 };
 
-let test = setInterval(func, 10);
-
-document.addEventListener("keydown", (event) => {
-  if (event.code === "ArrowDown") {
-    direction = "down";
-  } else if (event.code === "ArrowUp") {
-    direction = "up";
-  } else if (event.code === "ArrowRight") {
-    direction = "right";
+// Generate new food with random coordinates
+function newFood() {
+  let newFood = document.createElement("div");
+  newFood.classList.add("food");
+  if (snakeBody.length) {
+    foodX = randomPosition()[0];
+    foodY = randomPosition()[1];
   } else {
-    direction = "left";
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+  }
+
+  newFood.style.cssText = `grid-column: ${foodX};
+    grid-row: ${foodY};`;
+  grid.appendChild(newFood);
+}
+
+selectLevel.addEventListener("change", (e) => {
+  switch (e.target.value) {
+    case "1":
+      gameLevel = 500;
+      break;
+    case "2":
+      gameLevel = 250;
+      break;
+    case "3":
+      gameLevel = 125;
+      break;
+
+    default:
+      gameLevel = 500;
+      break;
   }
 });
 
-const resetBtn = document.querySelector(".reset");
+function stopInterval() {
+  clearInterval(loop);
+}
 
-resetBtn.addEventListener("click", () => {
-  x = 0;
-  y = 0;
-  direction = "right";
-  bait.style.top = `${generateRandomNum()}rem`;
-  bait.style.left = `${generateRandomNum()}rem`;
+function resetGame() {
+  clearInterval(loop);
+  loop = null;
+  snakeX = 10;
+  snakeY = 10;
+  directionX = 1;
+  directionY = 0;
+  document.querySelectorAll(".head").forEach((head) => {
+    head.remove();
+  });
+  document.querySelectorAll(".snake").forEach((snake) => {
+    snake.remove();
+  });
+  document.querySelectorAll(".food").forEach((food) => {
+    food.remove();
+  });
   score = 0;
-  test = setInterval(func, 10);
+  scoreBoard.innerText = score;
+  gamePlaying = false;
+}
+
+startBtn.addEventListener("click", () => {
+  startGame();
+  gamePlaying = true;
 });
+resetBtn.addEventListener("click", resetGame);
